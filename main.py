901e5180b64d7944e968d6d89f7cc7a09ac1b53f
Kam <58607149+spyderkam@users.noreply.github.com>
2025-05-05 10:26:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 #!usr/bin/env python3
 
 __author__ = "Kamyar Modjtahedzadeh"
-__date__ = "May 2, 2025"
+__date__ = "May 1, 2025 - May 5, 2025"
 
 """
     Let the origin be the center of the parent body at impact (𝑡 = 0).
 """
 
-from gdmpidc_tools import calculate_mass, cross_sectional_area, empirical_parameters, expansion_velocity, get_AM_value
+from gdmpidc_tools import calculate_mass, cross_sectional_area, empirical_parameters, expansion_velocity, get_AM_value, packing_density
 import numpy as np
 
 # M_EARTH = 5.972e+24       # [kg]
 # GRAV_CONST = 6.67430e-11  # [m^3·kg^-1·s^-2]
 parent_vel = 0              # [m·s^-1]
+parent_rad = 4.1439e+11     # [m]
 
 
 class Fragment:
@@ -21,8 +22,7 @@ class Fragment:
     def __init__(self, characteristic_length: float, pos: list, creation_type="collision") -> None:
         self.size = characteristic_length                         # characteristic length
         self.pos = pos                                            # (x, y, z)
-        # r = √(x^2 + y^2 + z^2)
-        self.radial = np.sqrt(pos[0]**2 + pos[1]**2 + pos[2]**2)
+        self.radial = np.sqrt(pos[0]**2 + pos[1]**2 + pos[2]**2)  # r = √(x^2 + y^2 + z^2)
 
         self.mass = calculate_mass(characteristic_length)
         self.area = cross_sectional_area(characteristic_length)
@@ -50,9 +50,12 @@ class Fragment:
 
 
 class Cloud:
-    def __init__(self, initRadius, characteristic_length, breakup_type="collision") -> None:
-        self.radius = initRadius
-        self.fragmentSize = characteristic_length
+    def __init__(self, characteristic_length, num_fragments, breakup_type="collision") -> None:
+        self.radius = parent_rad*packing_density(characteristic_length)**(-1/3)
+        #self.radius = initRadius
+        self.nFrag = num_fragments
+        self.fragSize = characteristic_length
+        self.breakup_type = breakup_type
 
     def cloud_radius(self, t: float) -> float:
         """
@@ -66,13 +69,16 @@ class Cloud:
         """
         return self.radius + t*expansion_velocity(parent_mass=1000, L_min=0.001, L_max=15.0)
 
-    #def sample_fragment(self, pos) -> Fragment:
-    #    return Fragment(self.characteristic_length, pos)
+    def sample_fragment(self, pos) -> Fragment:
+        return Fragment(self.fragSize, pos, creation_type=self.breakup_type)
 
     #def density(self) -> float:
     #    ρ0*np.exp(-0.5 * ((r - μ*self.radius)/(σ*self.radius))**2)
 
 
 if __name__ == "__main__":
-    sample_fragment = Fragment(0.01, [0, 0, 0])
-    print(type(sample_fragment))
+    #sample_fragment = Fragment(0.01, [0, 0, 0])
+    #print(type(sample_fragment))
+
+    cloud = Cloud(10,12)
+    
