fa538c3ede47d2e670b5c4f429d3602e82453bfb
Kam <58607149+spyderkam@users.noreply.github.com>
2025-05-15 11:19:32 -0500

    Update percent_inside.py

--- src/percent_inside.py
+++ src/percent_inside.py
@@ -5,19 +5,19 @@ __date__ = "May 14, 2025"
 
 # This script calculates the portion of fragments inside cloud radii and prints the average.
 
-nRuns = 2
-nFrags = 100
-s_Lc = 0.05
-m_Lc = 0.09
-l_Lc = 0.15
+nRuns = 10
+nFrags = 1000
+s_Lc = 0.05     # [cm]
+m_Lc = 0.09     # [cm]
+l_Lc = 0.15     # [cm]
 
 from gdmpidc import *
 from numpy import linalg, mean
 
 ## Inside and outside probabilities of s/m/l fragments.
-in_ratios_s, out_ratios_s = [], []
-in_ratios_m, out_ratios_m = [], []
-in_ratios_l, out_ratios_l = [], []
+in_ratios_s = []
+in_ratios_m = []
+in_ratios_l = []
 
 for _ in range(nRuns):
     ## Generate s/m/l clouds and fragments.
@@ -26,14 +26,14 @@ for _ in range(nRuns):
     largeCloud = Cloud(l_Lc, nFrags);  vec_r_l = largeCloud.sample_positions()
         
     ## Inside and outside numbers of s/m/l fragments.
-    inl_s, outl_s = [], []
-    inl_m, outl_m = [], []
-    inl_l, outl_l = [], []
+    inl_s = []
+    inl_m = []
+    inl_l = []
 
     fragment_data = {
-        'small': [smallCloud, inl_s, outl_s, in_ratios_s, out_ratios_s],
-        'medium': [medCloud, inl_m, outl_m, in_ratios_m, out_ratios_m],
-        'large': [largeCloud, inl_l, outl_l, in_ratios_l, out_ratios_l],
+        'small': [smallCloud, inl_s, in_ratios_s],
+        'medium': [medCloud, inl_m, in_ratios_m],
+        'large': [largeCloud, inl_l, in_ratios_l],
     }
 
     for i in range(len(vec_r_s)):
@@ -43,22 +43,18 @@ for _ in range(nRuns):
         
         for size_category in fragment_data.keys():
             fragment_data[size_category].insert(0, locals()[f"r_{size_category[0]}"])
-            r, cloud, inside, outside, _, _ = fragment_data[size_category]
+            r, cloud, inside, _, = fragment_data[size_category]
         
             if linalg.norm(r) <= cloud.updated_radius(t=0):
                 inside.append(r)
-            else:
-                outside.append(r)
-            
             fragment_data[size_category].pop(0)
 
     for size_category in fragment_data.keys():
-        _, inl, outl, in_ratios, out_ratios = fragment_data[size_category]
+        _, inl, in_ratios = fragment_data[size_category]
         in_ratios.append(len(inl)/cloud.nFrag)
-        out_ratios.append(len(outl)/cloud.nFrag)
 
 for size_category in fragment_data.keys():
-    _, _, _, in_ratios, out_ratios = fragment_data[size_category]
+    _, _, in_ratios = fragment_data[size_category]
     if size_category == "small":
         print(f"Small fragments: {mean(in_ratios)*100}% inside")
     elif size_category == "medium":
