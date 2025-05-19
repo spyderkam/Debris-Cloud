e2884863cccbd2e85af7f4768b204deddd0719cc
Kam <58607149+spyderkam@users.noreply.github.com>
2025-05-18 23:26:13 -0500

    Update gdmpidc_tools.py

--- src/gdmpidc_tools.py
+++ src/gdmpidc_tools.py
@@ -222,9 +222,9 @@ def empirical_parameters(Lc):
     """
         Define empirical parameters for the fragment based on its characteristic length.
         These parameters have been optimized to achieve the following coverage:
-          - Small fragments (Lc < 8 cm): ≳75% inside cloud radius
-          - Medium fragments (8 cm < Lc < 0.11 cm): ≳90% inside cloud radius
-          - Large fragments (Lc > 0.11 cm): ≳99% inside cloud radius
+          - Small fragments (Lc ≈ 5 cm): ≳75% inside cloud radius
+          - Medium fragments (Lc ≈ 9 cm): ≳90% inside cloud radius
+          - Large fragments (Lc ≈ 15 cm): ≳99% inside cloud radius
         
         Returns:
             tuple: (μ, ρ0, σ0, α, γ)
