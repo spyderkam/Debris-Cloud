dd2c73a33a5cc24bd7cda4154ed7ebce08fd10a9
Kam <kamyarmojtahed@gmail.com>
2025-05-21 03:16:13 +0000

    Renamed `func` to `cloud_intersect_line`

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ __date__ = "May 20, 2025"
 from src.gdmpidc import *
 import numpy as np
 
-inside_points = np.array([point for point in vec_r if np.linalg.norm(point) <= cloud.radius])
 
-def func(entry_point, exit_point, t):
+cloud = Cloud(characteristic_length=0.05, num_fragments=1000, breakup_type="collision")
+all_initial_positions = cloud.sample_positions()
+inside_points = [point for point in all_initial_positions if np.linalg.norm(point) <= cloud.radius]
+
+def cloud_intersect_line(entry_point, exit_point, t):
     """
-    Given the entry and exit points of the sphere at time 𝑡, this function calculates the slope and 𝑦-intercept of the
-    line that passes through these points.
+    Given the entry and exit points of the cloud at time 𝑡, compute the slope 
+    and 𝑦-intercept of the line that passes through these points.
 
     Args:
         entry_point (list): (x, y, z) coordinates of the entry point
