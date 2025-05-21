1c91b3be8a6c062396334ff95420089e34e792fc
Kam <kamyarmojtahed@gmail.com>
2025-05-21 03:05:38 +0000

    Initiated line function

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 #!usr/bin/env python3
 
 __author__ = "Kamyar Modjtahedzadeh"
-__date__ = "May 5, 2025"
+__date__ = "May 20, 2025"
 
 from src.gdmpidc import *
+import numpy as np
 
-print("Hello World!")
+inside_points = np.array([point for point in vec_r if np.linalg.norm(point) <= cloud.radius])
+
+def func(entry_point, exit_point, t):
+    """
+    Given the entry and exit points of the sphere at time 𝑡, this function calculates the slope and 𝑦-intercept of the
+    line that passes through these points.
+
+    Args:
+        entry_point (list): (x, y, z) coordinates of the entry point
+        exit_point (list): (x, y, z) coordinates of the exit point
+        t (float): Time since impact [s]
+
+    Returns:
+        tuple: (slope, y_intercept)
+    """
+    
+
 
 
 if __name__ == "__main__":
---
+++ src/line.py
@@ -0,0 +1,5 @@
+#!usr/bin/env python3
+
+__author__ = "Kamyar Modjtahedzadeh"
+
+# Eventually, this file will contain the code for the line probability at time 𝑡 = 0.
