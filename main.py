1832e680f30271c1aac5403bc4714505569d4907
Kam <kamyarmojtahed@gmail.com>
2025-05-27 15:49:12 -0500

    updates

--- main.py
+++ main.py
@@ -3,25 +3,31 @@
 __author__ = "Kamyar Modjtahedzadeh"
 __date__ = "May 27, 2025"
 
-from src.geometric_analysis import *
 from src.gdmpidc import *
 from src.gdmpidc_tools import *
+import time
+
+start_time = time.time()
 
 ϵ = 0.00001              # [m]
-parent_mass = 9.39e20    # [kg]
+#parent_mass = 9.39e20    # [kg]
+parent_mass = 10000
+N = cumulative_distribution
 
 def subrange_count(L1, L2, breakup_type: str = "collision") -> int:
     """Number of fragments between lengths L1 and L2 where L2 > L1."""
     if breakup_type == "collision":
-        return cumulative_distribution(L1, parent_mass) - cumulative_distribution(L2, parent_mass)
+        number_of_fragments = N(L1, parent_mass) - N(L2, parent_mass)
     elif breakup_type == "explosion":
-        return cumulative_distribution(L1, parent_mass, "explosion") - cumulative_distribution(L2, parent_mass, "explosion")
+        number_of_fragments = N(L1, parent_mass, "explosion") - N(L2, parent_mass, "explosion")
+    return int(number_of_fragments)
 
 def point_count(Lc: float, size_step: float = ϵ, breakup_type: str = "collision") -> int:
     if breakup_type == "collision":
-        return cumulative_distribution(Lc, parent_mass) - cumulative_distribution(Lc + size_step, parent_mass)
+        number_of_fragments = N(Lc, parent_mass) - N(Lc + size_step, parent_mass)
     elif breakup_type == "explosion":
-        return cumulative_distribution(Lc, parent_mass, "explosion") - cumulative_distribution(Lc + size_step, parent_mass, "explosion")
+        number_of_fragments = N(Lc, parent_mass, "explosion") - N(Lc + size_step, parent_mass, "explosion")
+    return int(number_of_fragments)
 
 small_cloud, ΔLc_small = dict(), 0.0001
 med_cloud, ΔLc_med     = dict(), 0.0005
@@ -39,3 +45,5 @@ while 0.11 < Lc <= 1.0:
     Lc += ΔLc_large
 
 cloud = {"small": small_cloud, "medium": med_cloud, "large": large_cloud}
+
+print(f"Processing time: {time.time() - start_time:.2f} seconds")
